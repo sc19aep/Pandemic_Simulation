7a4e102a6aaaf72ae3fe4ffb14a222c1baa3b658
sc19aep <sc19aep@leeds.ac.uk>
2022-01-30 13:34:46 +0200

    Added tasks and a way for all to move in a stright line to the first task

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,6 +49,7 @@ namespace Simulation
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // splitContainer1
--- Form1.cs
+++ Form1.cs
@@ -17,28 +17,23 @@ namespace Simulation
             public int x; //current x value
             public int y; //current y value
             public string status; //susceptible(blue), infected(red), removed(grey)
+            public Building house; //the house the person returns to at the end of the day
+            public List<Building> tasks;
         };
 
-        struct House
+        struct Building
         {
-            public int x;
-            public int y;
-            public List<Person> list;
+            public int x; //start point x location
+            public int y; //start point y location
         }
 
-        struct Shop
-        {
-            public int x; //store start x point
-            public int y; //store start y point
-        };
 
         Person p1, p2, p3, p4;
-        Shop s1;
-        House h1;
+        Building s1, h1; //shop 1 and house 1
 
-        List<House> houses = new List<House> {};
-        List<Shop> shops = new List<Shop> { };
-        List<Person> people = new List<Person> { };
+        List<Building> houses = new List<Building> {};
+        List<Building> shops = new List<Building> {};
+        List<Person> people = new List<Person> {};
 
         private void addHouse(int x, int y)
         {
@@ -50,21 +45,29 @@ namespace Simulation
             p1.x = x;
             p1.y = y;
             p1.status = "Blue";
+            p1.tasks = new List<Building> { };
+            p1.house = h1;
             people.Add(p1);
             //person 2
             p2.x = x + 10;
             p2.y = y;
             p2.status = "Blue";
+            p2.tasks = new List<Building> { };
+            p2.house = h1;
             people.Add(p2);
             //person 3
             p3.x = x;
             p3.y = y + 10;
             p3.status = "Blue";
+            p3.tasks = new List<Building> { };
+            p3.house = h1;
             people.Add(p3);
             //person 4
             p4.x = x + 10;
             p4.y = y + 10;
             p4.status = "Blue";
+            p4.tasks = new List<Building> { };
+            p4.house = h1;
             people.Add(p4);
         }
 
@@ -105,6 +108,19 @@ namespace Simulation
             }
         }
 
+        private void generateRoute()
+        {
+            Random rnd = new Random();
+
+            for (int i=0; i<people.Count(); i++)
+            {
+                for(int j = 0; j<3; j++)
+                {
+                    int num = rnd.Next(0, shops.Count());
+                    people[i].tasks.Add(shops[num]);
+                }
+            }
+        }
 
         public Form1()
         {
@@ -112,6 +128,7 @@ namespace Simulation
 
             //houses, shops and people
             generateMap();
+            generateRoute();
         }
 
         private void Start_Click(object sender, EventArgs e)
@@ -134,17 +151,28 @@ namespace Simulation
             {
                 Brush myBrush = new SolidBrush(Color.FromName(people[i].status));
                 e.Graphics.FillEllipse(myBrush, people[i].x, people[i].y, 10, 10);
-
             }
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (p1.x < s1.x + 35)
-                p1.x++;
-            if (p1.y < s1.y + 25)
-                p1.y++;
+            for(int i = 0; i<people.Count(); i++)
+            {
+                Person p1 = people[i];
+                int target_x = people[i].tasks[0].x;
+                int target_y = people[i].tasks[0].y;
+
+                if (p1.x < target_x + 35)
+                    p1.x = p1.x + 5;
+                if (p1.y < target_y + 25)
+                    p1.y = p1.y + 5;
+                if (p1.x > target_x + 35)
+                    p1.x = p1.x - 5;
+                if (p1.y > target_y + 25)
+                    p1.y = p1.y - 5;
+
+                people[i] = p1;
+            }
 
             splitContainer1.Panel2.Refresh();
         }
